06c3f6f50838b9fdf6f8e6d5155a05c5a4215549
Raviolz <gi.ravioli@gmail.com>
2026-04-10 16:07:49 +0200

    Initial commit

---
+++ BEu2w1d5/src/test/java/raviolz/BEu2w1d5/BEu2w1d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package raviolz.BEu2w1d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEu2w1d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
